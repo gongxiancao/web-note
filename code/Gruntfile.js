6a54a00dd460022527f5bef5420060d186e49565
gongxian <gongxian@gongxian-VirtualBox.(none)>
2014-01-14 13:47:06 +0800

    generate index file in build

--- code/Gruntfile.js
+++ code/Gruntfile.js
@@ -11,9 +11,11 @@ module.exports = function (grunt) {
             app: 'app',
             log: 'log',
             build: 'build',
+            compile: 'bin',
             server: 'server',
             root: __dirname
         },
+        pkg: grunt.file.readJSON('package.json'),
         buildClient: {
             production: [
                 'clean:build_log',
@@ -93,6 +95,31 @@ module.exports = function (grunt) {
                     'plato/note': ['app/js/modules/note/**/*.js']
                 }
             }
+        },
+        /**
+         * The `index` task compiles the `index.html` file as a Grunt template. CSS
+         * and JS files co-exist here but they get split apart later.
+         */
+        index: {
+
+            /**
+             * During development, we don't want to have wait for compilation,
+             * concatenation, minification, etc. So to avoid these steps, we simply
+             * add all script files directly to the `<head>` of `index.html`. The
+             * `src` property contains the list of included files.
+             */
+            build: {
+                dir: '<%= paths.build %>',
+                src: [
+                    '<%= paths.build %>/**/*.js',
+                    //'<%= paths.build %>/src/app/*.modules.js',
+                    //'<%= paths.build %>/src/**/*.js',
+                    //'<%= html2js.common.dest %>',
+                    //'<%= html2js.app.dest %>',
+                    //'<%= vendor_files.css %>',
+                    //'<%= recess.build.dest %>'
+                ]
+            },
         }
 
     };
@@ -129,6 +156,23 @@ module.exports = function (grunt) {
     var find = require('find'),
         path = require('path');
 
+    /**
+     * A utility function to get all app JavaScript sources.
+     */
+    function filterForJS(files) {
+        return files.filter(function(file) {
+            return file.match(/\.js$/) || (file.match(/\.modules/));
+        });
+    }
+
+    /**
+     * A utility function to get all app CSS sources.
+     */
+    function filterForCSS(files) {
+        return files.filter(function(file) {
+            return file.match(/\.css$/);
+        });
+    }
 
     grunt.registerTask('less2css', 'Compile less to css', function (buildType) {
 
@@ -159,6 +203,36 @@ module.exports = function (grunt) {
         }
         grunt.config.set('less', {build: {options: options, files: files}});
         grunt.task.run('less');
+    });
+
+    /**
+     * The index.html template includes the stylesheet and javascript sources
+     * based on dynamic names calculated in this Gruntfile. This task assembles
+     * the list into variables for the template to use and then runs the
+     * compilation.
+     */
+    grunt.registerMultiTask('index', 'Process index.html template', function() {
+        var dirRE = new RegExp('^(' + grunt.config.data.paths.build + '|' + grunt.config.data.paths.compile + ')\/', 'g');
+
+        var jsFiles = filterForJS(this.filesSrc).map(function(file) {
+            return file.replace(dirRE, '');
+        });
+
+        var cssFiles = filterForCSS(this.filesSrc).map(function(file) {
+            return file.replace(dirRE, '');
+        });
+
+        grunt.file.copy('app/index.html', this.data.dir + '/index.html', {
+            process: function(contents) {
+                return grunt.template.process(contents, {
+                    data: {
+                        scripts: jsFiles,
+                        styles: cssFiles,
+                        version: grunt.config.data.pkg.version
+                    }
+                });
+            }
+        });
     });
 
     grunt.registerTask('server', 'Start server.', function() {
