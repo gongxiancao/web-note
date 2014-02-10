7d592fc51c95c7efb4a4946a7f4cbdbbcb015170
gongxian <gongxian@gongxian-VirtualBox.(none)>
2014-02-10 17:03:51 +0800

    add watch grunt task phase 1

--- code/Gruntfile.js
+++ code/Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function (grunt) {
     var gruntConfig = {
         paths: {
             app: 'app',
+            vendor: 'vendor',
             log: 'log',
             build: 'build',
             build_log: 'build_log',
@@ -46,37 +47,47 @@ module.exports = function (grunt) {
             build: ['<%= paths.build %>']
         },
         copy: {
-            build_app_assets: {
+            // build_app_assets: {
+            //     files: [
+            //         {
+            //             src: [ '**' ],
+            //             dest: '<%= paths.build %>',
+            //             cwd: '<%= paths.app %>/',
+            //             expand: true
+            //         }
+            //     ]
+            // },
+            // build_vendor_assets: {
+            //     files: [
+            //         {
+            //             src: [ '<%= vendor_files.assets %>' ],
+            //             dest: '<%= paths.build %>/vendor',
+            //             cwd: '<%= paths.root %>',
+            //             expand: true,
+            //             flatten: true
+            //         }
+            //     ]
+            // },
+            build_appjs: {
                 files: [
                     {
-                        src: [ '**' ],
-                        dest: '<%= paths.build %>',
+                        src: [ '<%= app_files.js %>' ],
+                        dest: '<%= paths.build %>/',
                         cwd: '<%= paths.app %>/',
                         expand: true
                     }
                 ]
             },
-            build_vendor_assets: {
-                files: [
-                    {
-                        src: [ '<%= vendor_files.assets %>' ],
-                        dest: '<%= paths.build %>/vendor',
-                        cwd: '<%= paths.root %>',
-                        expand: true,
-                        flatten: true
-                    }
-                ]
-            },
-            build_vendor_js: {
-                files: [
-                    {
-                        src: [ '<%= vendor_files.js %>' ],
-                        dest: '<%= paths.build %>',
-                        cwd: '<%= paths.root %>',
-                        expand: true
-                    }
-                ]
-            },
+            // build_vendor_js: {
+            //     files: [
+            //         {
+            //             src: [ '<%= vendor_files.js %>' ],
+            //             dest: '<%= paths.build %>/vendor',
+            //             cwd: '<%= paths.vendor %>',
+            //             expand: true
+            //         }
+            //     ]
+            // },
         },
 
         jshint: {
@@ -141,8 +152,73 @@ module.exports = function (grunt) {
                     //'<%= recess.build.dest %>'
                 ]
             },
-        }
+        },
+        /**
+         * And for rapid development, we have a watch set up that checks to see if
+         * any of the files listed below change, and then to execute the listed
+         * tasks when they do. This just saves us from having to type "grunt" into
+         * the command-line every time we want to see what we're working on; we can
+         * instead just leave "grunt watch" running in a background terminal. Set it
+         * and forget it, as Ron Popeil used to tell us.
+         *
+         * But we don't need the same thing to happen for all the files.
+         */
+        delta: {
+            /**
+             * By default, we want the Live Reload to work for all tasks; this is
+             * overridden in some tasks (like this file) where browser resources are
+             * unaffected. It runs by default on port 35729, which your browser
+             * plugin should auto-detect.
+             */
+            options: {
+                livereload: true
+            },
 
+            /**
+             * When the Gruntfile changes, we just want to lint it. In fact, when
+             * your Gruntfile changes, it will automatically be reloaded!
+             */
+            gruntfile: {
+                files: 'Gruntfile.js',
+                tasks: [ 'jshint:jshint:gruntfile' ],
+                options: {
+                    livereload: false
+                }
+            },
+
+            jssrc: {
+                files: [
+                    '<%= app_files.js %>'
+                ],
+                tasks: [ 'jshint:jshint:src', 'karma:unit:run', 'copy:build_appjs' ]
+            },
+
+            // assets: {
+            //     files: [
+            //         'src/assets/**/*'
+            //     ],
+            //     tasks: [ 'copy:build_assets' ]
+            // },
+
+            // html: {
+            //     files: [ '<%= app_files.html %>' ],
+            //     tasks: [ 'index:build' ]
+            // },
+
+            // tpls: {
+            //     files: [
+            //         '<%= app_files.atpl %>',
+            //         '<%= app_files.ctpl %>'
+            //     ],
+            //     tasks: [ 'html2js' ]
+            // },
+
+            // less: {
+            //     files: [ 'app/**/*.less' ],
+            //      tasks: [ 'less2css' ]
+            // }
+
+        }
     };
 
     var userConfig = require('./build.config.js');
@@ -153,7 +229,17 @@ module.exports = function (grunt) {
         grunt.task.run(this.data);
     });
 
-    grunt.registerTask('default', ['buildClient:dev', 'server']);
+    /**
+     * In order to make it safe to just compile or copy *only* what was changed,
+     * we need to ensure we are starting from a clean, fresh build. So we rename
+     * the `watch` task to `delta` (that's why the configuration var above is
+     * `delta`) and then add a new task called `watch` that does a clean build
+     * before watching for changes.
+     */
+    grunt.renameTask('watch', 'delta');
+    grunt.registerTask('watch', [ 'buildClient:dev', 'server', /*'karma:unit',*/ 'delta'  ]);
+
+    grunt.registerTask('default', ['watch']);
 /*
     grunt.registerTask('buildSelenium', [
         'exec:build-automation',
@@ -258,8 +344,13 @@ module.exports = function (grunt) {
         });
     });
 
-    grunt.registerTask('server', 'Start server.', function() {
-        this.async();
+    grunt.registerTask('server', 'Start a custom web server.', function() {
         require(grunt.config.data.paths.root + '/' + grunt.config.data.paths.server + '/server.js');
+    });
+
+    grunt.registerTask('server-async', 'Start server async.', function() {
+        var done = this.async(),
+            server = require(grunt.config.data.paths.root + '/' + grunt.config.data.paths.server + '/server.js');
+        server.on('close', done);
     });
 };
--- code/build.config.js
+++ code/build.config.js
@@ -4,35 +4,35 @@
 module.exports = {
     app_files: {
         js: [
-            'build/*.js',
-            'build/modules/*/*.js',
-            'build/modules/*/**/*.js'
+            '*.js',
+            'modules/*/*.js',
+            'modules/*/**/*.js'
         ],
         css: [
             'vendor/jquery-ui/themes/base/jquery-ui.css',
             'vendor/select2/select2.css',
-            'build/resources/css/third-party.css',
-            'build/resources/css/note.css'
+            'resources/css/third-party.css',
+            'resources/css/note.css'
         ]
     },
     vendor_files: {
         js: [
-            'vendor/jquery/jquery.js',
-            'vendor/lodash/dist/lodash.js',
-            'vendor/angular/angular.js',
-            'vendor/jquery-ui/ui/jquery-ui.js',
-            'vendor/angular-animate/angular-animate.js',
-            'vendor/angular-ui-utils/modules/route/route.js',
-            'vendor/angular-ui-router/release/angular-ui-router.js',
-            'vendor/ng-grid/ng-grid-2.0.7.debug.js',
-            'vendor/log4javascript/log4javascript.js',
-            'vendor/angular-resource/angular-resource.js',
-            'vendor/angular-bootstrap/ui-bootstrap-tpls.js',
-            'vendor/d3/d3.js',
-            'vendor/nvd3/nv.d3.js',
-            'vendor/angular-translate/angular-translate.js',
-            'vendor/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
-            'vendor/angular-ui-tinymce/src/tinymce.js'
+            'jquery/jquery.js',
+            'lodash/dist/lodash.js',
+            'angular/angular.js',
+            'jquery-ui/ui/jquery-ui.js',
+            'angular-animate/angular-animate.js',
+            'angular-ui-utils/modules/route/route.js',
+            'angular-ui-router/release/angular-ui-router.js',
+            'ng-grid/ng-grid-2.0.7.debug.js',
+            'log4javascript/log4javascript.js',
+            'angular-resource/angular-resource.js',
+            'angular-bootstrap/ui-bootstrap-tpls.js',
+            'd3/d3.js',
+            'nvd3/nv.d3.js',
+            'angular-translate/angular-translate.js',
+            'angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+            'angular-ui-tinymce/src/tinymce.js'
         ],
         css: [
         ],
