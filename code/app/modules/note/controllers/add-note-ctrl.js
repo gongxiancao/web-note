7b6c0affe5b7c3b7c772e32e1d5c43c2955dc2b0
caogo <gong-xian.cao@hp.com>
2014-03-14 03:25:37 +0800

    add template to note

--- code/app/modules/note/controllers/add-note-ctrl.js
+++ code/app/modules/note/controllers/add-note-ctrl.js
@@ -1,6 +1,12 @@
 'use strict';
 
-angular.module('note').controller('AddNoteCtrl', [
-    function () {
+angular.module('note').controller('AddNoteCtrl', ['$scope',
+    function ($scope) {
+        $scope.isCategory = ($scope.model.template === 'category');
+        $scope.$watch('isCategory', function (value){
+            if(angular.isDefined(value)) {
+                $scope.model.template = value ? 'category' : 'note';
+            }
+        });
     }
 ]);
